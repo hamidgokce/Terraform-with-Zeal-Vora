bc9e18c80ac1a61e60fd5ae7e4faae6d64ca5e16
hamidgokce <hamidgokce@gmail.com>
2022-03-22 08:43:29 +0200

    section-3

---
+++ Section 3 - Terraform Provisioners/provisioner.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region     = "us-west-2"
+#   access_key = "AKIAQAJ6LG2JEBBST2FX"
+#   secret_key = "W0fvaeQz4r4VXf+m6/rRLPDl0yL8VLIbHjpjO6Vz"
+  access_key = "YOUR-KEY"
+  secret_key = "YOUR-KEY"
+}
+
+resource "aws_instance" "myec2" {
+    ami="ami-082b5a644766e0e6f"
+    instance_type = "t2.micro"
+    key_name = "terraform"
+
+   provisioner "remote-exec" {
+     inline = [
+       "sudo amazon-linux-extras install -y nginx1.12",
+       "sudo systemctl start nginx"
+     ]
+
+   connection {
+    # https://www.terraform.io/language/resources/provisioners/connection
+     type = "ssh"
+     user = "ec2-user"
+     private_key = file("./kplabs-terraform.pem")
+     host = self.public_ip
+   }
+   }
+}
+
+# https://www.udemy.com/course/terraform-beginner-to-advanced/learn/lecture/15725158?start=15#announcements
